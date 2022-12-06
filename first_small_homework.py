2d1ef1e1a3cea9f4a6414bfc91659b11bb6c38c2
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-12-06 14:02:57 +0100

    complete next tasks

--- first_small_homework.py
+++ first_small_homework.py
@@ -8,3 +8,25 @@ print(f'b = {b}')
 print(f'c = {c}')
 print(f'd = {d}')
 print('Well done')
+print('*'* 50)
+
+# ##---------------------------------
+
+fuelPer100Km = 6
+trip = 150
+tripCost = fuelPer100Km*1.5
+print(f'Trip cost = {tripCost}$')
+print('*' * 50)
+
+radius = 8
+pi = 3.14
+circleField = pi*radius**2
+circuitOfCircle = 2 * pi * radius
+
+print(f'Circle field = {circleField}')
+print(f'Circuit of circle = {circuitOfCircle}')
+print('*' * 50)
+
+pii = 3,14
+print(type(pii))
+print('*' * 50)
