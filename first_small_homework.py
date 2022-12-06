d53362331228f812f6fe07487456577f1a675039
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-12-06 16:53:33 +0100

    Finish first week

--- first_small_homework.py
+++ first_small_homework.py
@@ -1,33 +1,106 @@
-a = 2 * 17
-b = 66 % 5
-c = 9 ** 3
-d = 123321 / 11
-
-print(f'a = {a}')
-print(f'b = {b}')
-print(f'c = {c}')
-print(f'd = {d}')
-print('Well done')
-print('*'* 50)
+# a = 2 * 17
+# b = 66 % 5
+# c = 9 ** 3
+# d = 123321 / 11
+#
+# print(f'a = {a}')
+# print(f'b = {b}')
+# print(f'c = {c}')
+# print(f'd = {d}')
+# print('Well done')
+# print('*'* 50)
 
 # ##---------------------------------
 
-fuelPer100Km = 6
-trip = 150
-price = 6
-tripCost = trip * 6 / 100 * price
-print(f'Trip cost = {tripCost}$')
-print('*' * 50)
+# fuelPer100Km = 6
+# trip = 150
+# price = 6
+# tripCost = trip * 6 / 100 * price
+# print(f'Trip cost = {tripCost}$')
+# print('*' * 50)
+#
+# radius = 8
+# pi = 3.14
+# circleField = pi*radius**2
+# circuitOfCircle = 2 * pi * radius
+#
+# print(f'Circle field = {circleField}')
+# print(f'Circuit of circle = {circuitOfCircle}')
+# print('*' * 50)
+#
+# pii = 3,14
+# print(type(pii))
+# print('*' * 50)
 
-radius = 8
-pi = 3.14
-circleField = pi*radius**2
-circuitOfCircle = 2 * pi * radius
+# #-------------------------------------------------
 
-print(f'Circle field = {circleField}')
-print(f'Circuit of circle = {circuitOfCircle}')
-print('*' * 50)
+# inputHeight = float(input('What is your height in meters? '))
+# inputWeight = int(input('What is your weight in kg? '))
+# bmi = inputWeight / inputHeight ** 2
+# print(f'Your BMI is : {bmi}')
+# print('*'* 50)
+#
+# net_input = float(input('Input net value: '))
+# gross = net_input * 1.23
+#
+# print(f'Gross of value {net_input} is {gross}')
+# print('*'* 50)
 
-pii = 3,14
-print(type(pii))
-print('*' * 50)
+# height_input = float(input('Input h value: '))
+# site_A_input = float(input('Input a value: '))
+# site_B_input = float(input('Input b value: '))
+#
+# p = ((site_A_input + site_B_input) * height_input) / 2
+# print(f'Trapezoidal area = {p}')
+# print('*' * 50)
+
+# #-------------------------------------------------------
+#
+# number = int(input('Please input a number 0-9: '))
+#
+# if number % 2 == 0:
+#     print(f'Number {number} is even')
+# else:
+#     print(f'Number {number} is odd')
+# print('*' * 50)
+
+#
+# number1 = int(input('Please input a first number 0-9: '))
+# number2 = int(input('Please input a second number 0-9: '))
+#
+# if number1 > number2:
+#     print(f'Number1 "{number1}" is bigger')
+# elif number1 < number2:
+#     print('Number2 "{}" is bigger'.format(number2))
+# else:
+#     print(f'Number1 "{number1}" and number2 "{number2}" are equal')
+# print('*' * 50)
+
+# ''' # Funckja isnumeric() sprawdza czy string jest numerem
+# number1 = input('Please input a first number 0-9: ')
+# number2 = input('Please input a second number 0-9: ')
+#
+# if number1.isnumeric() and number2.isnumeric():
+#
+#     if number1 > number2:
+#         print(f'Number1 "{number1}" is bigger')
+#     elif number1 < number2:
+#         print('Number2 "{}" is bigger'.format(number2))
+#     else:
+#         print(f'Number1 "{number1}" and number2 "{number2}" are equal')
+# else:
+#     print('You must write a number')
+# print('*' * 50)
+# '''
+
+# temp = float(input('Please input value of temperature: '))
+#
+# if temp <= 10:
+#     print('Stay at home!')
+# elif temp > 10 and temp < 20:
+#     print('Get a jacket')
+# else:
+#     print('Have a fun!')
+# #---------------------------------------------------------
+
+
---
+++ first_weekly_homework.py
@@ -0,0 +1,26 @@
+# # First task to do.
+# # Convert degrees celsius to fahrenheit
+
+# celsius = float(input('Please input celsius degrees: '))
+# fahrenheit = 2 * celsius + 32
+# print(f'{celsius}°C = {fahrenheit}°F')
+
+# #************************************************
+
+# # Second task to do.
+# # Calculate the area of ​​a triangle by picking three vertices
+#
+# vertice_A_X = float(input('Please input x value in vertice A: '))
+# vertice_A_Y = float(input('Please input y value in vertice A: '))
+#
+# vertice_B_X = float(input('Please input x value in vertice B: '))
+# vertice_B_Y = float(input('Please input y value in vertice B: '))
+#
+# vertice_C_X = float(input('Please input x value in vertice C: '))
+# vertice_C_Y = float(input('Please input y value in vertice C: '))
+#
+# area = 0.5 * ((vertice_B_X - vertice_A_X) * (vertice_C_Y - vertice_A_Y) - (vertice_B_Y - vertice_A_Y) * (vertice_C_X - vertice_A_X))
+#
+# print(f'Area of triangle: {area}')
+
+# #****************************************************************************************
