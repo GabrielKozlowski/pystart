cb7a6310099c5e37e30602f68dfb48100aa7ac72
Gabrielkozlowski <gabriel89krk@gmail.com>
2023-02-07 14:15:28 +0100

    next two task

--- 3_third_weekly_homework.py
+++ 3_third_weekly_homework.py
@@ -85,5 +85,85 @@
 # print(animal_with_the_most_legs)
 # #*********************************************************************************
 
+# # >>>>>>>>>>>>>>> Fifth task to do. <<<<<<<<<<<<<<<<
+# # Create dictionary with info of your favorite animals (animal name, race).
+# # Find animal that has the longest name and print animal name and rase.
+#
+# home_animals = {
+#     'Tigra': 'dog',
+#     'Hultek': 'cat',
+#     'Chester': 'rabbit',
+#     'Diana': 'dog',
+#
+# }
+# animal_name = ''
+#
+# for name in home_animals.keys():
+#     if len(name) > len(animal_name):
+#         animal_name = name
+#
+# print(animal_name)
 
+# #**************************************************************
 
+# # >>>>>>>>>>>>>>> Sixth task to do. <<<<<<<<<<<<<<<<
+# # Create dictionary with info of your favorite countries (country name, capitol, language).
+# # Add a new country to dict and print new dictionary.
+
+#
+#
+# info = '''
+#
+# Welcome to dictionary of your favorite countries info.
+# Press number to function.
+#
+# 1 - Show dictionary info
+# 2 - Add new country to dictionary
+# 3 - Delete country from dictionary
+# 4 - Quit program
+#
+# '''
+# dictionary = []
+#
+# while True:
+#
+#     try:
+#         choices_number = int(input(info))
+#     except ValueError:
+#         print('Wrong number')
+#     else:
+#         if choices_number == 1:
+#             if len(dictionary) == 0:
+#                 print('Nothing in here')
+#             else:
+#                 for country in dictionary:
+#                     print(country)
+#
+#         elif choices_number == 2:
+#             country_name = input('Enter name of country: ')
+#             capitol_name = input('Enter name of capitol: ')
+#             language_name = input('Enter name of language: ')
+#             print()
+#             new_country_info = {
+#                 'country': country_name,
+#                 'capitol': capitol_name,
+#                 'language': language_name,
+#             }
+#
+#             if new_country_info not in dictionary:
+#                 dictionary.append(new_country_info)
+#             else:
+#                 print('The country is already in dictionary ')
+#
+#         elif choices_number == 3:
+#             country_to_delete = input('Enter country name to delete: ')
+#             for number, country in enumerate(dictionary):
+#                 if country_to_delete in country['country']:
+#                     del dictionary[number]
+#                 else:
+#                     pass
+#
+#         elif choices_number == 4:
+#             break
+
+# #*****************************************************************************************
