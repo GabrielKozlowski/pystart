bf8535dc3879dc43fd352a9366156f50783d725b
Gabrielkozlowski <gabriel89krk@gmail.com>
2023-02-07 12:19:37 +0100

    third and fourth task

--- 3_third_weekly_homework.py
+++ 3_third_weekly_homework.py
@@ -28,3 +28,62 @@
 # #*****************************************************************************
 
 
+# # >>>>>>>>>>>>>>> Third task to do. <<<<<<<<<<<<<<<<
+# # Create dictionary with info of your favorite movies (title, production year, director).
+# # Next delete Steven Spielberg movie.
+#
+# favorite_movies = {
+#     'Steven Spielberg':
+#         [
+#             {
+#                 'title': 'Saving Private Ryan',
+#                 'production_year': 1998,
+#             },
+#             {
+#                 'title': 'Minority Report',
+#                 'production_year': 2002,
+#             }
+#         ],
+#     'Christopher Nolan':
+#         [
+#             {
+#                 'title': 'Interstellar',
+#                 'production_year': 2014,
+#             },
+#             {
+#                 'title': 'Inception',
+#                 'production_year': 2010,
+#             }
+#         ]
+# }
+#
+# print(favorite_movies)
+# del favorite_movies['Steven Spielberg'][:]
+# print(favorite_movies)
+
+# #***************************************************************************
+
+# # >>>>>>>>>>>>>>> Fourth task to do. <<<<<<<<<<<<<<<<
+# # Create dictionary with info of your favorite animals (animal name, number of legs).
+# # Find animal that has the most legs and print animal name and value of legs.
+#
+# favorite_animals = {
+#     'dog': 4,
+#     'horse': 4,
+#     'spider': 8,
+#     'millipede': 100,
+#     'cat': 4,
+# }
+# value = 0
+# animal_with_the_most_legs = ''
+#
+# for animal, legs in favorite_animals.items():
+#     if legs > value:
+#         value = legs
+#         animal_with_the_most_legs = animal
+#
+# print(animal_with_the_most_legs)
+# #*********************************************************************************
+
+
+
