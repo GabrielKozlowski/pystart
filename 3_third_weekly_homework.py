79f92190117639b14e95e2c82276f8dd65bc85ed
Gabrielkozlowski <gabriel89krk@gmail.com>
2023-02-07 12:17:53 +0100

    First two task in third weekly homework

---
+++ 3_third_weekly_homework.py
@@ -0,0 +1,30 @@
+# # >>>>>>>>>>>>>>> First task to do. <<<<<<<<<<<<<<<<
+# # Create list of friends and print that list sorted alphabetically
+# ## Create list of friends names
+
+# friends_names = ['Mirek', 'Łukasz', 'Rafał', 'Paweł', 'Grzesiek']
+
+#  ## Create variable with sorted friends list
+# sorted_list = sorted(friends_names)
+
+# ## Print sorted list
+# print(sorted_list)
+# #***************************************************************************
+
+# # >>>>>>>>>>>>>>> Second task to do. <<<<<<<<<<<<<<<<
+# # Create dictionary with keys as fruits and value as price. Print value of apples and banana
+
+# products = {
+#     'Apples': 3.50,
+#     'Orange': 4.70,
+#     'Kivi': 1.20,
+#     'Banana': 1.75,
+#     'Watermelon': 6.0
+# }
+#
+# print(f"Apples price: {products['Apples']}")
+# print(f"Banana price: {products['Banana']}")
+#
+# #*****************************************************************************
+
+
